e783092f41d4983b65952055e7372eb670f57d06
saanvibajaj28-lgtm <saanvibajaj28@gmail.com>
2026-06-08 01:45:33 +0530

    day7

---
+++ day6/twosum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+vector<int> twoSum(vector<int> arr, int target) {
+        int n = arr.size();
+        vector<pair<int, int>> numsWithIndex;
+        for (int i = 0; i < n; i++) {
+            numsWithIndex.push_back({arr[i], i});
+        }
+        sort(numsWithIndex.begin(), numsWithIndex.end());
+
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int sum = numsWithIndex[left].first + numsWithIndex[right].first;
+            if (sum == target) {
+                return {numsWithIndex[left].second, numsWithIndex[right].second};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return {-1, -1};  }
+};
